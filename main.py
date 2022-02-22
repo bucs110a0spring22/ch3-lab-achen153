b4e271a75dd0aa27c0d18573e1b00a5c5a8c6913
Angela L L <98534270+achen153@users.noreply.github.com>
2022-02-22 02:45:13 +0000

    completion

--- main.py
+++ main.py
@@ -5,22 +5,45 @@ import random
 window = turtle.Screen()       # 2.  Create a screen
 window.bgcolor('lightblue')
 
-michelangelo = turtle.Turtle()    # 3.  Create two turtles
+mich = turtle.Turtle()    # 3.  Create two turtles
 leonardo = turtle.Turtle()
-michelangelo.color('orange')
+mich.color('orange')
 leonardo.color('blue')
-michelangelo.shape('turtle')
+mich.shape('turtle')
 leonardo.shape('turtle')
 
-michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
+mich.up()          # 4.  Pick up the pen so we don’t get lines
 leonardo.up()
-michelangelo.goto(-100,20)
+mich.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. your code goes here
+mich.speed(1)
+leonardo.speed(1)
+mich.down()
+leonardo.down()
+mich.forward(random.randrange(0,100))
+leonardo.forward(random.randrange(0,100))
+mich.goto(-100,20)
+leonardo.goto(-100,-20)
 
 
+for i in range(0,10):
+  x = random.randrange(0,10)
+  mich.forward(x)
+  leonardo.forward(x)
+  
 # Part B - complete part B here
-
+mich.clear()
+leonardo.clear()
+sides = [3,4,6,9,12]
+mich.down()
+length = 30
+for i in sides:
+  angle = 360/i
+  for num in range(i):
+    mich.right(angle)
+    mich.forward(length)
+  mich.clear()
 
 window.exitonclick()
